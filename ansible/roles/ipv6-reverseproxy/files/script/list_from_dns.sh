030a9d978d27d86d583f4be602ca9869b314203d
Polz <polz@polz.si>
2021-11-05 15:13:33 +0100

    list_from_dns.sh now works with cnames

--- ansible/roles/ipv6-reverseproxy/files/script/list_from_dns.sh
+++ ansible/roles/ipv6-reverseproxy/files/script/list_from_dns.sh
@@ -10,7 +10,8 @@ do
 done
 for HNAME in $(echo $HNAMES | sort | uniq);
 do
-    ENDIP=$(dig +short -t AAAA ${HNAME} | head -n 1)
+    # ENDIP=$(dig +short -t AAAA ${HNAME} | head -n 1) # this does not work for cnames
+    ENDIP=$(dig +nocmd ${HNAME} AAAA +noall +answer | grep -P 'IN\tAAAA\t[a-f0-9:]*$' | sed -e 's/.*AAAA\t//' | head -n 1)
     if [ -n "$ENDIP" ];
     then
         echo "$HNAME [${ENDIP}];";
